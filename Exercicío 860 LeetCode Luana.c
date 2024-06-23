529975e34eb9fb9b61c4dfe8bd2076592e37a96f
Luana Montilha Pinheiro <167804850+luanamontilhap@users.noreply.github.com>
2024-06-23 14:58:56 -0300

    Add files via upload
    Código feito em aula, apenas com uma básica função e o código principal.

---
+++ Exercicío 860 LeetCode Luana.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+bool lemonadeChange(int* bills, int billsSize) {
+    if (i>5){
+        return false;
+    }
+    for (int i; i<bills; i++){
+        
+    }
+}
+
+
+
+int main () {
+    int i;
+    int bills[i];
+
+    printf ("Valores de cada nota recebida: ");
+    scanf ("%d", &bills[i]);
+
+}
